c518b907627c209e81765c7a667fb11f68b11ea2
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2024-09-27 01:29:23 +0800

    ci: update_index.yml

---
+++ generate.mjs
@@ -0,0 +1,64 @@
+import fs from "fs";
+import { meta, sing } from "./description-map.mjs";
+
+function generate(client, path, geodata, ext) {
+  const dirs = fs.readdirSync(path, { encoding: "utf-8" });
+  const files = dirs.filter((file) => file.endsWith(ext));
+  const list = files.map((item) => {
+    const name = item.substring(0, item.length - ext.length);
+    const content = fs.readFileSync(path + item, { encoding: "utf-8" });
+    let count = 0;
+
+    if (ext === ".list") {
+      count = content.split("\n").length;
+    } else if (ext === ".json") {
+      count = JSON.parse(content).rules.reduce(
+        (p, c) =>
+          Object.values(c).reduce(
+            (p, c) => (Array.isArray(c) ? p + c.length : p + 1),
+            0
+          ) + p,
+        0
+      );
+    }
+
+    return {
+      name,
+      description: { meta, sing }[client][name],
+      type: geodata,
+      count,
+    };
+  });
+  return list;
+}
+
+function generateMeta() {
+  const geosite = generate("meta", "./meta/geo/geosite/", "geosite", ".list");
+  const geoip = generate("meta", "./meta/geo/geoip/", "geoip", ".list");
+  const content = JSON.stringify({
+    geosite:
+      "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/meta/geo/geosite/",
+    geoip:
+      "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/meta/geo/geoip/",
+    list: geosite.concat(geoip),
+  });
+
+  fs.writeFileSync("./meta.json", content, { encoding: "utf-8" });
+}
+
+function generateSing() {
+  const geosite = generate("sing", "./sing/geo/geosite/", "geosite", ".json");
+  const geoip = generate("sing", "./sing/geo/geoip/", "geoip", ".json");
+  const content = JSON.stringify({
+    geosite:
+      "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/sing/geo/geosite/",
+    geoip:
+      "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/refs/heads/sing/geo/geoip/",
+    list: geosite.concat(geoip),
+  });
+
+  fs.writeFileSync("./sing.json", content, { encoding: "utf-8" });
+}
+
+generateMeta();
+generateSing();
